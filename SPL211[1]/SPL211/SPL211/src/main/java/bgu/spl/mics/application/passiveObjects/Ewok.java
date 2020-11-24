5a01f0cb2bd1138328d33c5abaf963956496d14d
Ron Tohar <rontoh@pos.bgu.ac.il>
2020-11-24 21:00:28 +0200

    tests

--- SPL211[1]/SPL211/SPL211/src/main/java/bgu/spl/mics/application/passiveObjects/Ewok.java
+++ SPL211[1]/SPL211/SPL211/src/main/java/bgu/spl/mics/application/passiveObjects/Ewok.java
@@ -6,6 +6,7 @@ package bgu.spl.mics.application.passiveObjects;
  * <p>
  * You may add fields and methods to this class as you see fit (including public methods).
  */
+
 public class Ewok {
 	int serialNumber;
 	boolean available;
